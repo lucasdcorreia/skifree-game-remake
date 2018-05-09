48a21b38162bc209bd6a7604f6c9c776e6655bd3
lucasdcorreia <ldc@icomp.ufam.edu.br>
2018-05-09 17:54:19 -0400

    PHP1 - Formulário com action

---
+++ PHP1/script.php
@@ -0,0 +1,7 @@
+<?php
+    echo "<p>Nome: " . $_GET['nome'] . "</p>";
+    echo "<p>Email: " . $_GET['email'] . "</p>";
+    echo "<p>Website: " . $_GET['website'] . "</p>";
+    echo "<p>Mensagem: <br>" . $_GET['mensagem'] . "</p>";
+
+?>
