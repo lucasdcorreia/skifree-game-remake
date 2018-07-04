f6fa5d43b50c35b488a53950169bc1165e371c1a
lucasdcorreia <ldc@icomp.ufam.edu.br>
2018-07-04 03:52:02 -0400

    trabalho final

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 Repositório do trabalho da disciplina Programação Web do Instituto de Computação da UFAM, ministrada pelo professor David Fernandes.
 
 Observações:
+A pontuação do jogador é enviada para actionSave do jogoController por meio ds função 
+"registrarPontuacao()" implementada no código javascript do jogo, "javascript.js" na 
+pasta web/SkiFree/js.
+Esta função executa uma requisição ajax para o url "http://localhost:8888/progweb/yii/advanced/frontend/web/index.php?r=jogo/save",
+dependendo do sistema esse url precise ser alterado.
 
 
--- yii/database/icomp.sql
+++ yii/database/icomp.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:8889
--- Tempo de geração: 10/06/2018 às 02:57
--- Versão do servidor: 5.6.35
--- Versão do PHP: 7.1.8
+-- Generation Time: Jul 04, 2018 at 09:50 AM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `icomp`
+-- Database: `icomp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `curso`
+-- Table structure for table `curso`
 --
 
 CREATE TABLE `curso` (
@@ -33,23 +33,50 @@ CREATE TABLE `curso` (
   `descricao` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `curso`
+--
+
+INSERT INTO `curso` (`id_curso`, `nome`, `sigla`, `descricao`) VALUES
+(3, 'Sistemas de Informação', 'IE15', 'O curso de Bacharelado em Sistemas de Informação.'),
+(4, 'Ciência da Computação', 'IE08', 'Curso de Bacharelado em Ciência da Computação.'),
+(5, 'Engenharia de Software', 'IE17', 'Engenharia de Software'),
+(7, 'Administração', 'FA01', 'Curso de administração da UFAM.');
+
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `jogada`
+-- Table structure for table `jogada`
 --
 
 CREATE TABLE `jogada` (
   `id_jogada` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `pontuacao` int(11) NOT NULL,
-  `data_hora` varchar(45) NOT NULL
+  `data_hora` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `jogada`
+--
+
+INSERT INTO `jogada` (`id_jogada`, `id_user`, `pontuacao`, `data_hora`) VALUES
+(1, 6, 6000, '2018-06-28 00:57:40'),
+(2, 6, 6000, '2018-06-28 01:11:32'),
+(3, 6, 6000, '2018-06-28 01:45:44'),
+(4, 7, 820, '2018-06-28 02:50:24'),
+(5, 7, 1039, '2018-06-28 02:57:24'),
+(6, 7, 3418, '2018-06-28 03:04:26'),
+(7, 7, 2000, '2018-06-28 03:05:02'),
+(8, 7, 2000, '2018-06-28 03:11:57'),
+(9, 2, 750, '2018-06-28 03:19:12'),
+(10, 2, 870, '2018-07-03 14:17:11'),
+(11, 7, 1314, '2018-07-04 02:13:36');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `migration`
+-- Table structure for table `migration`
 --
 
 CREATE TABLE `migration` (
@@ -58,7 +85,7 @@ CREATE TABLE `migration` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Fazendo dump de dados para tabela `migration`
+-- Dumping data for table `migration`
 --
 
 INSERT INTO `migration` (`version`, `apply_time`) VALUES
@@ -68,7 +95,7 @@ INSERT INTO `migration` (`version`, `apply_time`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -85,29 +112,41 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Índices de tabelas apagadas
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `id_curso`) VALUES
+(2, 'user2', 'EKoApfjzgVMNOftYaiEpC0jHRJfwQHQg', '$2y$13$YJbtCvFWLI30H.bB7pPG9e8zlFS6/m55.Bvp2kPv5vTMw2io/Px.W', NULL, 'user2@email.com', 10, 0, 0, 3),
+(3, 'user3', 'ou6WmQ6nqH3TEGroYLIzfTSZGCOYhwn4', '$2y$13$imO4OuaBw2WvRxGJjVW.6.ECZVVg4/uqThFtk744mIaqkw.oJzpeC', NULL, 'user3@email.com', 10, 0, 0, 3),
+(4, 'user4', 'U-292Txm4mPmE7r0feSw_QVPzgVZCdUO', '$2y$13$1T6cc0Jqe8kB8lBAfdXJQu2d.Diia/.YvZOj3c8CNEeJqf6onwDO.', NULL, 'user4@email.com', 10, 0, 0, 3),
+(5, 'user6', 'T7htNForfVsoZt6A9ggM4FNCxDdkQ_Hi', '$2y$13$/Jh0WhCo/TlIKaVS5B3vq.GoC/7db5Z96B4GdrPRePmNIoopdpPQy', NULL, 'user6@email.com', 10, 0, 0, 5),
+(6, 'user7', '33Tw5_vk1sUD9-XFhe9Zv26fOecaDjv9', '$2y$13$Doj0Rz8n45Kfq6xyypvC9eKMJ1Gr/qa9v.IgfK.2lJ65bHVi2Xt9u', NULL, 'user7@email.com', 10, 1530125130, 0, 5),
+(7, 'user5', 'NqxuH_tD-ZEMmFkloTkoVOk70HPhEtcp', '$2y$13$qsMtbH49sCpeOjMIeCjWxOaEfCGXkx65UiQjtP7aK/y2m7i8j131O', NULL, 'user5@email.com', 10, 1530168600, 1530168600, 4);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Índices de tabela `curso`
+-- Indexes for table `curso`
 --
 ALTER TABLE `curso`
   ADD PRIMARY KEY (`id_curso`);
 
 --
--- Índices de tabela `jogada`
+-- Indexes for table `jogada`
 --
 ALTER TABLE `jogada`
   ADD PRIMARY KEY (`id_jogada`);
 
 --
--- Índices de tabela `migration`
+-- Indexes for table `migration`
 --
 ALTER TABLE `migration`
   ADD PRIMARY KEY (`version`);
 
 --
--- Índices de tabela `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
@@ -116,24 +155,24 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
 
 --
--- AUTO_INCREMENT de tabelas apagadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `curso`
+-- AUTO_INCREMENT for table `curso`
 --
 ALTER TABLE `curso`
-  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
--- AUTO_INCREMENT de tabela `jogada`
+-- AUTO_INCREMENT for table `jogada`
 --
 ALTER TABLE `jogada`
-  MODIFY `id_jogada` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_jogada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
--- AUTO_INCREMENT de tabela `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
